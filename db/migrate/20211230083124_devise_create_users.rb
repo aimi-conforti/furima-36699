936bfa28a55b5903085bc975811f4877fd8001e0
aimi-conforti <nozaru213@yahoo.co.jp>
2022-01-08 19:48:43 +0900

    新規登録、ログインページの実装

--- db/migrate/20211230083124_devise_create_users.rb
+++ db/migrate/20211230083124_devise_create_users.rb
@@ -4,11 +4,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :family_name,        null: false
+      t.string :first_name,         null: false
+      t.string :kana_family_name,   null: false
+      t.string :kana_first_name,    null: false
+      t.date   :date_of_birth,      null: false
+
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token 
       t.datetime :reset_password_sent_at
 
       ## Rememberable
