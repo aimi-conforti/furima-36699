67a00451afb13be56ee6eb30813021ae9e8fc874
aimi-conforti <nozaru213@yahoo.co.jp>
2022-01-09 16:44:55 +0900

    テストコード修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "must consist of more than six alphanumeric characters"
+         
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "please enter in full width characters"} do
     validates :family_name
     validates :first_name
+  end
+
+  with_options presence: true, format: {with: /\A[ァ-ヴー]+\z/u, message: "please enter in full width katakana" } do
     validates :kana_family_name
     validates :kana_first_name
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,25 +21,50 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
+    it 'passwordが英数混合6文字以上でなくては登録できない' do
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password must consist of more than six alphanumeric characters"
+    end
     it 'family_nameが空では登録できない' do
       @user.family_name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "Family name can't be blank"
+    end
+    it 'family_nameが半角文字では登録できない' do
+      @user.family_name = 'yamada'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Family name please enter in full width characters"
     end
     it 'first_nameが空では登録できない' do
       @user.first_name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "First name can't be blank"
     end
+    it 'first_nameが半角文字では登録できない' do
+      @user.first_name = 'yamada'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "First name please enter in full width characters"
+    end
     it 'kana_family_nameが空では登録できない' do
       @user.kana_family_name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "Kana family name can't be blank"
     end
+    it 'kana_family_nameが全角カタカナ以外では登録できない' do
+      @user.kana_family_name = 'やまだ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana family name please enter in full width katakana"
+    end
     it 'kana_first_nameが空では登録できない' do
       @user.kana_first_name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "Kana first name can't be blank"
+    end
+    it 'kana_first_nameが全角カタカナ以外では登録できない' do
+      @user.kana_first_name = 'やまだ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana first name please enter in full width katakana"
     end
     it 'date_of_birthが空では登録できない' do
       @user.date_of_birth = ''
