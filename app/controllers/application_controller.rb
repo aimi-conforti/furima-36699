2fedf1f64d3d96903bd1040c2ac5fd36f4526e02
aimi-conforti <nozaru213@yahoo.co.jp>
2022-01-09 12:59:38 +0900

    バリデーション実装②

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name, :first_name, :kana_family_name, :kana_first_name, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :date_of_birth])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :family_name
+    validates :first_name
+    validates :kana_family_name
+    validates :kana_first_name
+  end
+
   validates :nickname,         presence:true
-  validates :family_name,      presence:true
-  validates :first_name,       presence:true
-  validates :kana_family_name, presence:true
-  validates :kana_first_name,  presence:true
   validates :date_of_birth,    presence:true
 end
