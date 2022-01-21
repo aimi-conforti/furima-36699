37379b52dd72c3aaf1324cd328e4441450b76e4c
aimi-conforti <nozaru213@yahoo.co.jp>
2022-01-21 17:48:59 +0900

    FactoryBot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    new_items             {'商品'}
+    items_explain         {'新商品'}
+    category_id           {'メンズ'}
+    sales_status_id       {'新品・未使用'}
+    postage_id            {'着払い（購入者負担）'}
+    prefecture_id         {'北海道'}
+    shipping_date_id      {'1~2日で発送'}
+    sell_price            {300}
   end
 end
