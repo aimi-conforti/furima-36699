96c0163f6e632933b7b5ed48b106a05217587a28
aimi-conforti <nozaru213@yahoo.co.jp>
2022-03-22 13:01:16 +0900

    デバッグし直し3

--- app/javascript/card_token.js
+++ app/javascript/card_token.js
@@ -12,10 +12,8 @@ const pay = () => {
       exp_month: formData.get("order_address[card_exp_month]"),
       exp_year: `20${formData.get("order_address[card_exp_year]")}`,
     };
-    console.log(card)
 
     Payjp.createToken(card, (status, response) => {
-      console.log(status)
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
@@ -27,7 +25,7 @@ const pay = () => {
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
 
-     // document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").submit();
     })
   })
 }
